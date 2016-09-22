41c188d8ba772dfd05b593afa1566fff5b81c9b7
Albert <buboakstor@mail.ru>
2016-09-22 14:27:20 +0300

    add new rule

---
+++ BrainGame/GameLogics/RuleMultiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels;
+
+namespace GameLogics
+{
+    public class RuleMultiplication : BaseRule
+    {
+        public RuleMultiplication(ref FieldViewModel fieldModel) : base(ref fieldModel)
+        {
+        }
+    }
+}
---
+++ BrainGame/GameLogics/RuleSubtraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels;
+
+namespace GameLogics
+{
+    public class RuleSubtraction : BaseRule
+    {
+        public RuleSubtraction(ref FieldViewModel fieldModel) : base(ref fieldModel)
+        {
+        }
+    }
+}
